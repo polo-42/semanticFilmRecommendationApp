896d011786ba4d2b00f0273c61ee3f13e2cb3cfe
polo42 <paul.mulard@etu.unige.ch>
2023-04-16 03:00:05 +0200

    feat: use dbpedia to fetch informations

--- app.py
+++ app.py
@@ -1,10 +1,55 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
+from db.utils import filmGraph
+import json
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+graphdb = filmGraph()
 
 @app.route("/")
 def index():
-    return render_template('home.html')
+    return render_template('connexion.html')
 
-if __name__=='__main__':    
+@app.route("/home", methods=['GET','POST'])
+def home():
+    if request.method == 'POST':
+        session['firstname'] = request.form['firstname']
+        session['lastname'] = request.form['lastname']
+        user = renderuser()
+        if not graphdb.isExisting(user):
+            user['id'] = (user['firstname']+user['lastname']).replace(' ','')
+            graphdb.createUser(user)
+
+        session['iduser'] = graphdb.getUserId(user)
+    
+    if "genres" in request.args:
+        graphdb.addFavoriteTypes(session['iduser'], json.loads(request.args['genres']))
+
+    user = renderuser()
+    genres = graphdb.getFavoriteTypes(session['iduser'])
+    films = graphdb.getFilms(genres)
+    return render_template("home.html",user=user, films=films)
+
+@app.route('/select')
+def select():
+    user = renderuser()
+    genres = graphdb.getGenres()
+    return render_template("select.html",user=user, genres=genres)
+
+@app.route('/more')
+def more():
+    if "film" in request.args:
+        film = graphdb.getMoreInformations(request.args['film'])
+        return render_template('filmInformations.html',film=film)
+
+    return 'error', 404
+
+def renderuser():
+    return {
+        'firstname': session['firstname'] if 'firstname' in session else 'John',
+        'lastname': session['lastname'] if 'lastname' in session else 'Doe'
+    }
+    
+
+if __name__=='__main__':
     app.run(debug=True)
--- db/utils.py
+++ db/utils.py
@@ -1,4 +1,6 @@
 from rdflib import Graph
+from rdflib.plugins.stores.sparqlstore import SPARQLStore
+import functools
 
 class filmGraph():
     
@@ -7,25 +9,200 @@ class filmGraph():
         self.graph.parse('appFilm/db/data.ttl')
         self.graph.close()
 
-    def getFilm(self) -> object:
-        request = """
-            PREFIX : <http://bda/tp2/paulmulard/>
-            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-            SELECT ?title ?description ?genre ?copyright
-            WHERE { 
-                ?film a :Film . 
-                ?film rdfs:label ?title .
-            OPTIONAL {
-                ?film rdfs:comment ?description
-            }
-            OPTIONAL {
-                ?film :isOfType ?genre
-            }
-            OPTIONAL {
-                ?film :baseCopyRight ?copyright
-            }}"""
-        
-        return self.graph.query(request)
+        self.dbpedia = Graph("SPARQLStore")
+        self.dbpedia.open("http://dbpedia.org/sparql")
 
-g = filmGraph()
-print([f for f in g.getFilm()])
+        request = filmGraph.GETCATEGORIES
+        result = self.dbpedia.query(request)
+        self.genres = {
+            genre['uri'].split("/")[-1]: {
+                'uri': genre['uri'].split("/")[-1], 
+                'name': genre['label']
+            } for genre in result 
+        }
+
+    def isExisting(self, user: dict) -> bool:
+         request = filmGraph.GETUSER.format(user=user)
+
+         user = self.graph.query(request)
+         return True if (len(user) >= 1) else False
+    
+    def createUser(self, user: dict) -> None:
+        request = filmGraph.CREATEUSER.format(user=user)
+
+        self.graph.update(request)
+        self.save()
+    
+    def save(self) -> None:
+        self.graph.serialize(destination="db/data.ttl")
+        self.graph = Graph()
+        self.graph.parse('appFilm/db/data.ttl')
+        self.graph.close()
+    
+    def getFilms(self, genres):
+        films = []
+
+        if len(genres) <= 1 :
+            limit = 10
+        elif len(genres) <= 3 :
+            limit = 5
+        else :
+            limit = 2
+        
+        for genre in genres:
+            request = filmGraph.GETFILMS.format(genre=genre,limit=limit)
+            print(request)
+            result = self.dbpedia.query(request)
+            for film in result:
+                films.append({
+                    'uri': film['uri'],
+                    'title': film['title'],
+                    'description': film['description'],
+                    'genre': self.genres[genre]['name']
+                })
+        return films
+
+    def getGenres(self):
+        return self.genres.values()
+    
+    def getMoreInformations(self, uriFilm):
+        request = filmGraph.GETMOREINFORMATIONS.format(uriFilm=uriFilm)
+        result = self.dbpedia.query(request)
+        film = {}
+        for f in result:
+            film = {
+                'title': f['name'],
+                'description': f['description'],
+                'directors': self.getMoreInformationsOnDirectors(uriFilm),
+                'actors': self.getMoreInformationsOnActors(uriFilm)
+            }
+        return film
+
+    def getMoreInformationsOnDirectors(self, uriFilm):
+        request = filmGraph.GETDIRECTORS.format(uriFilm=uriFilm)
+        result = self.dbpedia.query(request)
+        directors = []
+        for director in result :
+            directors.append({
+                'name': director['name']
+            })
+        return result
+
+    def getMoreInformationsOnActors(self, uriFilm):
+        request = filmGraph.GETACTORS.format(uriFilm=uriFilm)
+        result = self.dbpedia.query(request)
+        actors = []
+        for actor in result :
+            actors.append({
+                'name': actor['name'] if 'name' in actor else actor['actor']
+            })
+        return result
+
+    def getUserId(self, user):
+        request = filmGraph.GETUSER.format(user=user)
+        result = self.graph.query(request)
+        
+        return [u['uri'] for u in result][0]
+
+    def addFavoriteTypes(self, userUri, genresUri):
+        favoriteTypes = functools.reduce(
+            lambda genre1, genre2: f'{genre1}, {genre2}',
+            map(lambda genre : f'dbr:{genre}', genresUri)
+        )
+
+        request = filmGraph.ADDTYPES.format(userUri=userUri,favoriteTypes=favoriteTypes)
+        self.graph.update(request)
+        self.save()
+    
+    def getFavoriteTypes(self, userUri):
+        request = filmGraph.GETTYPES.format(userUri=userUri)
+        print(request)
+        result = self.graph.query(request)
+        return [g['genre'] for g in result]
+
+    GETUSER = """
+            PREFIX : <http://bda/tp2/paulmulard/>
+            PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+            SELECT DISTINCT ?uri 
+            WHERE {{
+                ?uri a :User;
+                    foaf:firstName '{user[firstname]}';
+                    foaf:lastName '{user[lastname]}' .
+            }}"""
+    
+    CREATEUSER = """
+            PREFIX : <http://bda/tp2/paulmulard/>
+            PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+            INSERT DATA {{ 
+
+                :{user[id]} a :User;
+                    foaf:firstName '{user[firstname]}';
+                    foaf:lastName '{user[lastname]}' .
+            }}
+        """
+    
+    GETFILMS = """
+        SELECT DISTINCT ?uri ?title ?description
+        WHERE {{
+            ?uri a dbo:Film;
+                rdfs:label ?title;
+                dbo:abstract ?description;
+                dbo:wikiPageWikiLink dbr:{genre} .
+            FILTER (lang(?description) = 'fr')
+            FILTER (lang(?title) = 'fr')
+        }} 
+        LIMIT {limit}
+    """
+
+    GETCATEGORIES = """
+        SELECT DISTINCT ?uri ?label
+        WHERE {{
+            ?uri dct:subject dbc:Film_genres;
+                rdfs:label ?label .
+            FILTER (lang(?label) = 'fr')
+        }}
+    """
+    GETMOREINFORMATIONS = """
+        SELECT ?name ?description
+        WHERE {{
+            <{uriFilm}> dbo:abstract ?description;
+                rdfs:label ?name .
+            FILTER (lang(?description) = 'fr')
+            FILTER (lang(?name) = 'fr')
+        }}
+    """
+
+    GETDIRECTORS = """
+        SELECT DISTINCT ?name
+        WHERE {{
+            <{uriFilm}> dbo:director/rdfs:label ?name .
+            FILTER (lang(?name) = 'fr')
+        }}
+    """
+
+    GETACTORS = """
+        SELECT DISTINCT ?actor ?name
+        WHERE {{
+            <{uriFilm}> <http://dbpedia.org/property/starring> ?actor .
+            OPTIONAL {{
+                ?actor rdfs:label ?name .
+                FILTER(lang(?name) = 'fr')
+            }}
+        }}
+    """
+
+    ADDTYPES = """
+            PREFIX : <http://bda/tp2/paulmulard/>
+            PREFIX dbr: <http://dbpedia.org/resource/>
+            INSERT DATA {{ 
+                <{userUri}> :favoriteType {favoriteTypes} .
+            }}
+    """
+
+    GETTYPES = """
+            PREFIX : <http://bda/tp2/paulmulard/>
+            SELECT ?genre
+            WHERE {{
+                <{userUri}> :favoriteType ?genre .
+            }}
+    """
